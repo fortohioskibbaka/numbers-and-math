58d1f0586e30d69576df81433160baa829d8a902
Parker Hammond <339720419@GoTVDSB.ca>
2024-09-09 09:29:09 -0400

    fin

--- numbers and math/Program.cs
+++ numbers and math/Program.cs
@@ -1,5 +1,11 @@
 ﻿//parker
-Console.WriteLine("my age is"); 
+Console.WriteLine("what is my age is 2024-2008"); 
 Console.WriteLine(2024 - 2008);
 Console.WriteLine("whats the avrage of 14,15,16"); 
-Console.WriteLine((14 + 15 + 16) ); 
+Console.WriteLine((14 + 15 + 16) / 3
+   );
+Console.WriteLine((14.0 + 15.0 + 16.0 + 17.0) / 4);
+Console.WriteLine("what is the area of the circle with a radius of 3cm");
+Console.WriteLine((3.14 * 3 * 3));
+Console.WriteLine("the C# knows to add with the proper use of brackets in the line of code");
+Console.ReadLine();
